7ab41f79e1f0e83fc3311423eeb2007856cc52c8
Navskion <97650047+Navskion@users.noreply.github.com>
2023-01-28 16:32:40 +0800

    login page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const  { rmSync } = require('fs');
 /////////////////////// CONFIGURATIONS
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname + "/public"));
+app.set("views", path.join(__dirname + "/views"));
 PORT = 4000;
 app.listen(PORT);
 app.use(bodyParser.urlencoded({ extended: false }));
