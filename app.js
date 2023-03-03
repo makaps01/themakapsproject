a7f7fbc743a1f4b9aaea08200a9ee27a94a21956
aRVee <97650047+Navskion@users.noreply.github.com>
2023-03-03 23:15:21 +0800

    change format of date

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/login', (req, res)=>{
 // POST REQUEST FOR LOGIN
 app.post('/login', (req, res)=>{
     var {school_id, email, p_word} = req.body;
-   pool.query("SELECT * FROM tbl_sti_register WHERE email=?",[email],(err, result)=>{
+   pool.query("SELECT * date_format(birthday, %m/%d/%Y) AS birthday_format FROM tbl_sti_register WHERE email=?",[email],(err, result)=>{
     if(err) throw err;
     console.log(result)
     if(result.length==0){
