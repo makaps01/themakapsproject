3a4d67d5782bdf7e060be593c7f250e4e7b01729
makaps01 <123719289+makaps01@users.noreply.github.com>
2023-06-19 17:01:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -18,18 +18,6 @@ const fileUpload = require("express-fileupload");
 const nodemailer = require('nodemailer');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './img')
-    },
-    filename: (req, file, cb) => {
-        console.log(file)
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-});
-
-
-const upload = multer({storage: storage})
 
 const {
     user_isLoggedIn,
